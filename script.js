f9601cc1ec1be27c0577f70530ee2842140af484
m.sofiichuk <koyap@ukr.net>
2022-08-30 07:20:03 +0300

    done

---
+++ script.js
@@ -0,0 +1,9 @@
+let mainContent = document.querySelector(".mainContent");
+
+let sideMenu = document.querySelector('.sideMenu');
+
+sideMenu.addEventListener('click', function (event) {
+    mainContent.style.backgroundColor = event.target.id;
+    document.querySelector(".text").innerText = event.target.id;
+    document.querySelector(".text").style.right = '875px';
+});
